e631380d04f85d5eb4d83c94a8790d1f5ef20401
itsmanoj2003 <manojprabhakaran524@gmail.com>
2025-04-13 20:29:28 +0530

    Removed deprecated MongoDB options from server.js

--- server.js
+++ server.js
@@ -11,12 +11,9 @@ app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() => console.log('MongoDB connected'))
-.catch((err) => console.error('MongoDB connection error:', err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error('MongoDB connection error:', err));
 
 app.use('/ecomart', routes);
 
